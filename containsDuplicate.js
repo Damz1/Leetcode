2c7912e9d4ea686ed8c3276f8de42b3837ce2a33
David Maizil <davidmaizil200@gmail.com>
2023-09-25 13:12:32 +0100

    contains duplicates

---
+++ containsDuplicate.js
@@ -0,0 +1,14 @@
+let containsDuplicate = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 4, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 2, 3, 4, 4]));
