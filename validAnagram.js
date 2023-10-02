ac251e02f612931757e1bc07ae6178bfa3f3b702
David Maizil <davidmaizil200@gmail.com>
2023-10-02 15:07:37 +0100

    twoSum

--- validAnagram.js
+++ validAnagram.js
@@ -1,12 +1,12 @@
 let isAnagram = function (s, t) {
   sArray = s.split("");
   tArrat = t.split("");
-
+  
   sortedS = sArray.sort();
   sortedT = tArrat.sort();
-
+  
   joinedS = sortedS.join("");
   joinedT = sortedT.join("");
-
+  
   return joinedS === joinedT;
 };
