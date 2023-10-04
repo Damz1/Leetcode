03f110ce5d614707355ae4bcb6349e2014fc5868
David Maizil <davidmaizil200@gmail.com>
2023-10-04 16:26:58 +0100

    find top 1 frequent

--- topKfreuent.js
+++ topKfreuent.js
@@ -1,16 +1,22 @@
 let topKFrequent = function (nums, k) {
-  //create an object with keys of nums
-  //count how many times each one appear
-
-  const object = {};
+  const countObj = {};
   for (let num of nums) {
-    if (object[num]) {
-      object[num]++;
+    if (countObj[num]) {
+      countObj[num]++;
     } else {
-      object[num] = 1;
+      countObj[num] = 1;
     }
   }
-  console.log(object);
+  console.log(countObj); // { '1': 3, '2': 2, '3': 1 }
+
+  for (let key in countObj) {
+    let maxValue = 0;
+    if (countObj[key] > maxValue) {
+      output = [countObj[key]];
+    }
+  }
+
+  console.log("output", output);
 };
 
 console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
@@ -20,3 +26,7 @@ Example 1:
 Input: nums = [1,1,1,2,2,3], k = 2
 Output: [1,2]
 */
+
+//create an object with how many times each key appears
+//repeat k times:
+//find the largest value and push its key to an array
