8487504d314a044670ac6abf3305946d0bbf66b5
David Maizil <davidmaizil200@gmail.com>
2024-02-20 15:52:41 +0000

    simplfied mid search

--- AlgoMonster/BinarySearch.js
+++ AlgoMonster/BinarySearch.js
@@ -3,7 +3,7 @@ function binarySearch(arr, target) {
   let right = arr.length - 1;
 
   while (left <= right) {
-    let mid = left + Math.floor((right - left) / 2);
+    let mid = Math.floor((left + right) / 2);
 
     if (arr[mid] === target) {
       return mid;
