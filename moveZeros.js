eb576e0c8e14a81ab4025e9adbe0efff2fa3ba3d
David Maizil <davidmaizil200@gmail.com>
2024-02-24 16:00:46 +0000

    move zeroes added more test cases

--- moveZeros.js
+++ moveZeros.js
@@ -5,13 +5,13 @@ const moveZeros = (nums) => {
   let L = 0;
   let R = 1;
   while (R < n) {
-    console.log("nums", nums);
     if (nums[L] !== 0) {
       L++;
       R++;
     } else if (nums[R] == 0) {
       R++;
     } else {
+      //if left is zero and right is not, switch them
       let temp = nums[R];
       nums[R] = nums[L];
       nums[L] = temp;
--- moveZeros.test.js
+++ moveZeros.test.js
@@ -14,4 +14,31 @@ describe("moveZeros", () => {
     moveZeros(nums);
     expect(nums).toEqual(expected);
   });
+  it("should move all zeroes to the end while maintaining relative order for [1, 2, 0, 0, 4]", () => {
+    const nums = [1, 2, 0, 0, 4];
+    const expected = [1, 2, 4, 0, 0];
+    moveZeros(nums);
+    expect(nums).toEqual(expected);
+  });
+
+  it("should move all zeroes to the end while maintaining relative order for [0, 0, 0, 0, 0]", () => {
+    const nums = [0, 0, 0, 0, 0];
+    const expected = [0, 0, 0, 0, 0];
+    moveZeros(nums);
+    expect(nums).toEqual(expected);
+  });
+
+  it("should move all zeroes to the end while maintaining relative order for [1, 0, 2, 0, 3, 0]", () => {
+    const nums = [1, 0, 2, 0, 3, 0];
+    const expected = [1, 2, 3, 0, 0, 0];
+    moveZeros(nums);
+    expect(nums).toEqual(expected);
+  });
+
+  it("should move all zeroes to the end while maintaining relative order for [5, 4, 3, 2, 1]", () => {
+    const nums = [5, 4, 3, 2, 1];
+    const expected = [5, 4, 3, 2, 1];
+    moveZeros(nums);
+    expect(nums).toEqual(expected);
+  });
 });
