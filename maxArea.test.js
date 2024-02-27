0830ddc741758bf563890495665881e15ffb0933
David Maizil <davidmaizil200@gmail.com>
2024-02-27 15:46:19 +0000

    passing more tests maxArea

--- maxArea.test.js
+++ maxArea.test.js
@@ -5,4 +5,11 @@ describe("maxArea", () => {
     let input = [1, 8, 6, 2, 5, 4, 8, 3, 7];
     expect(maxArea(input)).toBe(49);
   });
+  test("should return 1 for [1,1]", () => {
+    expect(maxArea([1, 1])).toBe(1);
+  });
+
+  test("should return 16 for [4,3,2,1,4]", () => {
+    expect(maxArea([4, 3, 2, 1, 4])).toBe(16);
+  });
 });
