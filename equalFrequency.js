5aa658c3ec0594f4b3c518481847dc7a2b8ec48a
David Maizil <davidmaizil200@gmail.com>
2024-02-27 01:10:41 +0000

    further changes to equalFrequncy to adjust for all test cases

--- equalFrequency.js
+++ equalFrequency.js
@@ -4,20 +4,35 @@ const equalFrequency = (word) => {
     frequency[char] = (frequency[char] || 0) + 1;
   }
 
+  //create an array of frequencies
   const counts = Object.values(frequency);
-  const maxCount = Math.max(...counts);
-  const minCount = Math.min(...counts);
+  //create a uniqe set of those values
+  const uniqueCounts = new Set(counts);
 
-  const maxCountFrequency = counts.filter((count) => count === maxCount).length;
-  const minCountFrequency = counts.filter((count) => count === minCount).length;
-
-  if (maxCount === minCount && maxCount === 1) {
+  //if there is only 1 value that is 1, return true
+  if (uniqueCounts.size === 1 && counts.length === 1) {
     return true;
   }
 
+  //if there are more then 2 values in the uniqe set, we cant equalize
+  if (uniqueCounts.size > 2) {
+    return false;
+  }
+
+  const [minCount, maxCount] = Array.from(uniqueCounts).sort((a, b) => a - b);
+  const minCountFrequency = counts.filter((count) => count === minCount).length;
+  const maxCountFrequency = counts.filter((count) => count === maxCount).length;
+
+  //expect(equalFrequency("abc")).toBe(true);
+  if (minCount === 1 && uniqueCounts.size === 1) {
+    return true;
+  }
+
+  // If there is only one character with a lower frequency and it occurs only once, or
+  // if there is only one character with a higher frequency and removing one occurrence of that character can equalize the frequencies
   if (
-    (maxCount - minCount === 1 && maxCountFrequency === 1) ||
-    (minCount === 1 && minCountFrequency === 1)
+    (minCount === 1 && minCountFrequency === 1 && minCount !== maxCount) ||
+    (maxCount - minCount === 1 && maxCountFrequency === 1)
   ) {
     return true;
   }
--- equalFrequency.test.js
+++ equalFrequency.test.js
@@ -4,7 +4,7 @@ describe("equalFrequency", () => {
   test("should return true", () => {
     expect(equalFrequency("abcc")).toBe(true);
   });
-  test("should return false for same frequency of chars", () => {
+  test("should return false for same frequency of chars appearing more than once", () => {
     expect(equalFrequency("aacc")).toBe(false);
   });
   test("should return true for same frequency 1 of all chars", () => {
@@ -12,5 +12,11 @@ describe("equalFrequency", () => {
   });
   test("should return true for same frequency 1 of all chars", () => {
     expect(equalFrequency("bbac")).toBe(true);
+  });
+  test("should return true for same frequency 1 of all chars", () => {
+    expect(equalFrequency("babbdd")).toBe(false);
+  });
+  test("should return true for same frequency 1 of all chars", () => {
+    expect(equalFrequency("zz")).toBe(true);
   });
 });
