2a79b06a01299f4a6dea3e647121a0f3f4908be3
David Maizil <davidmaizil200@gmail.com>
2024-03-01 11:34:46 +0000

    added test for two different size lists

--- mergeTwoLists.test.js
+++ mergeTwoLists.test.js
@@ -13,10 +13,16 @@ describe("mergeTwoLists", () => {
     let output = [0];
     expect(mergeTwoLists(list1, list2)).toEqual(output);
   });
-  test("given an empty list, should return the head of the none empty node", () => {
+  test("should merge correctly given two arrays of same size", () => {
     let list1 = [1, 2, 4];
     let list2 = [1, 3, 4];
     let output = [1, 1, 2, 3, 4, 4];
+    expect(mergeTwoLists(list1, list2)).toEqual(output);
+  });
+  test("should merge correctly given two arrays in different sizes", () => {
+    const list1 = [1, 3, 5];
+    const list2 = [2, 4, 6, 7, 8];
+    const output = [1, 2, 3, 4, 5, 6, 7, 8];
     expect(mergeTwoLists(list1, list2)).toEqual(output);
   });
 });
