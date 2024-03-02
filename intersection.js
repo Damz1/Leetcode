0bd99963713e7255478bd51b3eb5f341964ba146
David Maizil <davidmaizil200@gmail.com>
2024-03-02 14:00:50 +0000

    intersction refactored to two pointers and O(nlogn) time

--- intersection.js
+++ intersection.js
@@ -1,15 +1,31 @@
 const intersection = (nums1, nums2) => {
+  let sortedNums1 = nums1.sort((a, b) => a - b);
+  let sortedNums2 = nums2.sort((a, b) => a - b);
+
+  let lengthOfShortesArray =
+    sortedNums1 < sortedNums2 ? sortedNums1.length : sortedNums2.length;
+  if (lengthOfShortesArray === 0) {
+    return [];
+  }
+
+  let pointer1 = 0;
+  let pointer2 = 0;
   let result = [];
 
-  for (let i = 0; i < nums1.length; i++) {
-    for (let j = 0; j < nums2.length; j++) {
-      if (nums1[i] == nums2[j]) {
-        result.push(nums1[i]);
-        break;
+  while (pointer1 < lengthOfShortesArray && pointer2 < lengthOfShortesArray) {
+    if (sortedNums1[pointer1] === sortedNums2[pointer2]) {
+      if (!result.includes(sortedNums1[pointer1])) {
+        result.push(sortedNums1[pointer1]);
       }
+      pointer1++;
+      pointer2++;
+    } else if (sortedNums1[pointer1] < sortedNums2[pointer2]) {
+      pointer1++;
+    } else {
+      pointer2++;
     }
   }
-  return [...new Set(result)];
+  return result;
 };
 
 module.exports = { intersection };
