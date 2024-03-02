6ed020e96120a685ce404f4eebbeb0f6cea323ad
David Maizil <davidmaizil200@gmail.com>
2024-03-02 15:16:50 +0000

    added large input test cases

--- intersection.js
+++ intersection.js
@@ -3,7 +3,10 @@ const intersection = (nums1, nums2) => {
   let sortedNums2 = nums2.sort((a, b) => a - b);
 
   let lengthOfShortesArray =
-    sortedNums1 < sortedNums2 ? sortedNums1.length : sortedNums2.length;
+    sortedNums1.length < sortedNums2.length
+      ? sortedNums1.length
+      : sortedNums2.length;
+
   if (lengthOfShortesArray === 0) {
     return [];
   }
@@ -12,20 +15,19 @@ const intersection = (nums1, nums2) => {
   let pointer2 = 0;
   let result = [];
 
-  while (pointer1 < lengthOfShortesArray && pointer2 < lengthOfShortesArray) {
-    if (sortedNums1[pointer1] === sortedNums2[pointer2]) {
-      if (!result.includes(sortedNums1[pointer1])) {
-        result.push(sortedNums1[pointer1]);
-      }
+  while (pointer1 < sortedNums1.length && pointer2 < sortedNums2.length) {
+    if (sortedNums1[pointer1] < sortedNums2[pointer2]) {
       pointer1++;
+    } else if (sortedNums1[pointer1] > sortedNums2[pointer2]) {
       pointer2++;
-    } else if (sortedNums1[pointer1] < sortedNums2[pointer2]) {
-      pointer1++;
     } else {
+      result.push(sortedNums1[pointer1]);
+      pointer1++;
       pointer2++;
     }
   }
-  return result;
+  return [...new Set(result)];
 };
 
+intersection([1, 2, 2, 1], [2, 2]);
 module.exports = { intersection };
--- intersection.test.js
+++ intersection.test.js
@@ -5,6 +5,7 @@ describe("intersection", () => {
     let nums1 = [1, 2, 2, 1];
     let nums2 = [2, 2];
     let output = [2];
+
     expect(intersection(nums1, nums2)).toEqual(output);
   });
 
@@ -45,8 +46,20 @@ describe("intersection", () => {
 
   test("should return [1] for arrays with all elements being the same", () => {
     let nums1 = [1, 1, 1];
-    let nums2 = [1, 1, 1];
+    let nums2 = [2, 1, 1];
     let output = [1];
+    expect(intersection(nums1, nums2)).toEqual(output);
+  });
+  test("should return currect with large inputs", () => {
+    let nums1 = [
+      43, 85, 49, 2, 83, 2, 39, 99, 15, 70, 39, 27, 71, 3, 88, 5, 19, 5, 68, 34,
+      7, 41, 84, 2, 13, 85, 12, 54, 7, 9, 13, 19, 92,
+    ];
+    let nums2 = [
+      10, 8, 53, 63, 58, 83, 26, 10, 58, 3, 61, 56, 55, 38, 81, 29, 69, 55, 86,
+      23, 91, 44, 9, 98, 41, 48, 41, 16, 42, 72, 6, 4, 2, 81, 42, 84, 4, 13,
+    ];
+    let output = [2, 3, 9, 13, 41, 83, 84];
     expect(intersection(nums1, nums2)).toEqual(output);
   });
 });
