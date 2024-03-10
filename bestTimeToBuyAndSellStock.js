b190d9c7a85f5e38b6b0c13564cbeaee281a452d
David Maizil <davidmaizil200@gmail.com>
2024-03-10 17:18:26 +0000

    Added test cases

--- bestTimeToBuyAndSellStock.js
+++ bestTimeToBuyAndSellStock.js
@@ -1,24 +1,18 @@
 const maxProfit = function (prices) {
   if (prices.length <= 1) {
-    return 0; // If there's only one price or no price, we cannot make any profit
+    return 0;
   }
 
-  let minPrice = prices[0]; // Initialize minPrice to the first price
-  let maxProfit = 0; // Initialize maxProfit to 0
-  console.log(minPrice, maxProfit);
+  let minPrice = prices[0];
+  let maxProfit = 0;
 
   for (let i = 1; i < prices.length; i++) {
     minPrice = Math.min(minPrice, prices[i]);
 
     maxProfit = Math.max(maxProfit, prices[i] - minPrice);
-    console.log(minPrice, prices[i], maxProfit);
   }
 
   return maxProfit;
 };
-
-// Example usage:
-const prices = [7, 1, 5, 3, 6, 4];
-console.log(maxProfit(prices)); // Output: 5
 
 module.exports = { maxProfit };
--- bestTimeToBuyAndSellStock.test.js
+++ bestTimeToBuyAndSellStock.test.js
@@ -4,4 +4,19 @@ describe("maxProfit", () => {
   test("should return 5", () => {
     expect(maxProfit([7, 1, 5, 3, 6, 4])).toBe(5);
   });
+  test("should return 0 when prices array is empty", () => {
+    expect(maxProfit([])).toBe(0);
+  });
+
+  test("should return 0 when all prices are the same", () => {
+    expect(maxProfit([5, 5, 5, 5, 5])).toBe(0);
+  });
+
+  test("should return 0 when prices are in descending order", () => {
+    expect(maxProfit([6, 5, 4, 3, 2, 1])).toBe(0);
+  });
+
+  test("should return 4 when there's only one buying opportunity", () => {
+    expect(maxProfit([1, 2, 3, 4, 5])).toBe(4);
+  });
 });
