c12836654296714b8454f82ce908fdbae06c1b7e
David Maizil <davidmaizil200@gmail.com>
2024-03-13 13:21:22 +0000

    max Path Sum tree

--- treeMinValue.js
+++ treeMinValue.js
@@ -6,16 +6,27 @@
 //   }
 // }
 
-const treeMinValue = (root) => {
-  let min = root.val;
-  let queue = [root];
+//BFS AND QUEUE
+// const treeMinValue = (root) => {
+//   let min = root.val;
+//   let queue = [root];
 
-  while (queue.length > 0) {
-    const current = queue.shift();
-    min = Math.min(min, current.val);
+//   while (queue.length > 0) {
+//     const current = queue.shift();
+//     min = Math.min(min, current.val);
 
-    if (current.left) queue.push(current.left);
-    if (current.right) queue.push(current.right);
-  }
-  return min;
+//     if (current.left) queue.push(current.left);
+//     if (current.right) queue.push(current.right);
+//   }
+//   return min;
+// };
+
+//DFS and Recursion
+const treeMinValue2 = (root) => {
+  if (!root) return Infinity;
+  return Math.min(
+    root.val,
+    treeMinValue2(root.left),
+    treeMinValue2(root.right)
+  );
 };
