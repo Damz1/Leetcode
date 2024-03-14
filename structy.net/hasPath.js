ba1493937c74aa7b35d48976a190a43822fcfe08
David Maizil <davidmaizil200@gmail.com>
2024-03-14 15:23:32 +0000

    has path from a to b in a graph

---
+++ structy.net/hasPath.js
@@ -0,0 +1,33 @@
+//Check whether a path exist from src to dst in graph
+
+const graph = {
+  f: ["g", "i"],
+  g: ["h"],
+  h: [],
+  i: ["g", "k"],
+  j: ["i"],
+  k: [],
+};
+
+//iterative using DFS
+const hasPath = (graph, src, dst) => {
+  const stack = [src];
+
+  //iterate while stack is not empty
+  while (stack.length) {
+    const current = stack.pop();
+    //if current node is equal to dst there is a path
+    if (current === dst) {
+      return true;
+    }
+    //if it's not, iterate over the node's neighbour and push them to the stack
+    for (let neighbour of graph[current]) {
+      stack.push(neighbour);
+    }
+  }
+  //finished the iteration with no matching to dst return false
+  return false;
+};
+
+console.log(hasPath(graph, "f", "k")); //true
+console.log(hasPath(graph, "f", "j")); // false
