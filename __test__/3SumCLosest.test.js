22e4b763b1f6502e9fd445b8b9686fb96214d32e
David Maizil <davidmaizil200@gmail.com>
2024-03-16 15:36:03 +0000

    added more test cases to 3 sum

--- __test__/3SumCLosest.test.js
+++ __test__/3SumCLosest.test.js
@@ -1,10 +1,31 @@
 const { threeSumClosest } = require("../3SumClosest");
 
-describe("first", () => {
-  test("should first", () => {
+describe("threeSumClosest", () => {
+  test("should return the closest sum to the target when the target is positive", () => {
     let input = [-1, 2, 1, -4];
     let target = 1;
     let output = 2;
+    expect(threeSumClosest(input, target)).toBe(output);
+  });
+
+  test("should return the closest sum to the target when the target is negative", () => {
+    let input = [1, 1, 1, 0];
+    let target = -100;
+    let output = 2;
+    expect(threeSumClosest(input, target)).toBe(output);
+  });
+
+  test("should return the closest sum to the target when the array contains only zeros", () => {
+    let input = [0, 0, 0];
+    let target = 1;
+    let output = 0;
+    expect(threeSumClosest(input, target)).toBe(output);
+  });
+
+  test("should return the closest sum to the target when the array contains positive and negative numbers", () => {
+    let input = [-1, 2, 1, -4, 5];
+    let target = 5;
+    let output = 5;
     expect(threeSumClosest(input, target)).toBe(output);
   });
 });
